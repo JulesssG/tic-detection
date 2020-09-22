a5b71b899fd2d47431537b535ff08e1c28067464
Jules Gottraux <jules.gottraux@epfl.ch>
2020-09-22 09:48:59 +0200

    Add processing of big sample (10 min)

--- utils.py
+++ utils.py
@@ -12,15 +12,15 @@ def video2tensor(filename, vid_root='data/', tensor_root='data/tensors/'):
 class VideoLoader:
     def __init__(self, filename, duration=np.inf, batch_size=64, grayscale=False, **kwargs):
         self.filename = filename
-        self.cap = cv2.VideoCapture(filename)
         self.gray = grayscale
         self.batch_size = batch_size
-        self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.fps = round(self.cap.get(cv2.CAP_PROP_FPS))
+        cap = cv2.VideoCapture(filename)
+        self.total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.fps = round(cap.get(cv2.CAP_PROP_FPS))
         self.duration_frames = min(self.total_frames, np.ceil(duration*self.fps/batch_size)*batch_size)
         self.duration = self.duration_frames/self.fps
-        self.width  = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         if 'scale' in kwargs:
             self.scale = True
             self.original_width  = self.width
@@ -35,20 +35,41 @@ class VideoLoader:
         while cap.isOpened():
             ret, frame = cap.read()
             if ret:
-                
-                if self.gray:
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                if self.scale:
-                    frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
-                    
-                frames.append(frame)
+                frames.append(self.frame_transform(frame))
+            else:
+                cap.release()
+        
+        return np.array(frames)
+    
+    def get_random_frames(self, frames_ratio):
+        nframes = int(self.total_frames * frames_ratio)
+        frames = []
+        cap = cv2.VideoCapture(self.filename)
+        frame_ids = np.random.choice(np.arange(self.total_frames), 
+                                     size=nframes, 
+                                     replace=False)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            current_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+            if ret:
+                if current_frame in frame_ids:
+                    frames.append(self.frame_transform(frame))
             else:
                 cap.release()
         
         return np.array(frames)
             
 
+    def frame_transform(self, frame):
+        if self.gray:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if self.scale:
+            frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
+            
+        return frame
+    
     def __iter__(self):
+        self.__cap = cv2.VideoCapture(self.filename)
         self.current_frame = 0
         self.stop = False
         return self
@@ -58,19 +79,13 @@ class VideoLoader:
             raise StopIteration()
         
         frames = []
-        while self.cap.isOpened():
-            ret, frame = self.cap.read()
+        while self.__cap.isOpened():
+            ret, frame = self.__cap.read()
             if ret:
-                
-                if self.gray:
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                if self.scale:
-                    frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
-                    
-                frames.append(frame)
+                frames.append(self.frame_transform(frame))
                 self.current_frame += 1
             else:
-                self.cap.release()
+                self.__cap.release()
                 self.stop = True
                 break
             
