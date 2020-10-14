b5f39228d0a52d9f2aa09020726c729219b2d0e3
Jules Gottraux <jules.gottraux@epfl.ch>
2020-10-14 11:14:30 +0200

    Generalize SpatialConvAE, transforms can be chained in custom_pca

--- autoencoders.py
+++ autoencoders.py
@@ -37,20 +37,21 @@ class OneAutoEncoder(nn.Module):
         return x.view(x.shape[0], *self.shape)
     
 class SpatialConvAE(nn.Module):
-    def __init__(self, inchannels, ncomp, nl=nn.ReLU):
+    def __init__(self, inchannels, ncomp, nl=nn.ReLU, chans=[128, 128, 64]):
         super().__init__()
         self.ncomp = ncomp
+        self.chans = chans
         
-        self.encoder_convs = nn.Sequential(nn.Conv2d(inchannels, 128, kernel_size=26, stride=5), nl(), # 47
-                                           nn.Conv2d(128, 128, kernel_size=11, stride=3), nl(), # 13
-                                           nn.Conv2d(128, 64, kernel_size=6), nl()) # 8
+        self.encoder_convs = nn.Sequential(nn.Conv2d(inchannels, chans[0], kernel_size=26, stride=5), nl(), # 47
+                                           nn.Conv2d(chans[0], chans[1], kernel_size=11, stride=3), nl(), # 13
+                                           nn.Conv2d(chans[1], chans[2], kernel_size=6), nl()) # 8
         
         self.encoder_lin = nn.Linear(64*8*8, ncomp)
         self.decoder_lin = nn.Linear(ncomp, 64*8*8)
         
-        self.decoder_convs = nn.Sequential(nn.ConvTranspose2d(64, 128, kernel_size=6), nl(),
-                                           nn.ConvTranspose2d(128, 128, kernel_size=11, stride=3), nl(),
-                                           nn.ConvTranspose2d(128, inchannels, kernel_size=26, stride=5))
+        self.decoder_convs = nn.Sequential(nn.ConvTranspose2d(chans[2], chans[1], kernel_size=6), nl(),
+                                           nn.ConvTranspose2d(chans[1], chans[0], kernel_size=11, stride=3), nl(),
+                                           nn.ConvTranspose2d(chans[0], inchannels, kernel_size=26, stride=5))
         
         
     def forward(self, x):
@@ -58,7 +59,7 @@ class SpatialConvAE(nn.Module):
         x = x.view(x.shape[0], -1)
         x = self.encoder_lin(x)
         x = self.decoder_lin(x)
-        x = x.view(x.shape[0], 64, 8, 8)
+        x = x.view(x.shape[0], chans[2], 8, 8)
         x = self.decoder_convs(x)
         
         return x
