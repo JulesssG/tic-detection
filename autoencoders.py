1dfc91753dbf6d6ce64b066befe80cd9a42afa37
Jules Gottraux <jules.gottraux@epfl.ch>
2020-11-03 09:21:53 +0100

    Report colab change of TemporalConvAE

--- autoencoders.py
+++ autoencoders.py
@@ -65,43 +65,43 @@ class SpatialConvAE(nn.Module):
         return x
 
 class TemporalConvAE(nn.Module):
-    def __init__(self, inchannels, nlayers, layerchans):
+    def __init__(self, inchannels, nlayers, layerchans, low_dim_rep=None):
         super().__init__()
         self.inchannels = inchannels
         self.layerchans = layerchans
         c1 = c2 = c3 = c4 = c5 = layerchans
+        self.low_dim_rep = low_dim_rep
 
-        conv_params = [(inchannels, c1, 8, 2), # (1, c1, 29, 125, 125)
-                       (c1, c2, 7, (1, 2, 2)), # (1, c2, 23, 60, 60)
-                       (c2, c3, 8, (1, 2, 2)), # (1, c3, 16, 27, 27)
-                       (c3, c4, 7, (1, 2, 2)), # (1, c4, 10, 11, 11)
-                       (c4, c5, 5, (1, 2, 2))] # (1, c5, 6, 4, 4)
+        conv_params = {2: [(inchannels, c1, 8, 2),          # (1, c1, 5, 125, 125)
+                           (c1, c2, (5, 7, 7), (1, 2, 2))], # (1, c2, 1, 60, 60)
+
+                       3: [(inchannels, c1, 8, 2),          # (1, c1, 5, 125, 125)
+                           (c1, c2, (3, 7, 7), (1, 2, 2)),  # (1, c2, 3, 60, 60)
+                           (c2, c3, (3, 8, 8), (1, 2, 2))]  # (1, c3, 1, 27, 27)
+        }
 
         encoder_modules = []
-        for params in conv_params[:nlayers]:
+        for params in conv_params[nlayers]:
             encoder_modules.append(nn.Conv3d(params[0], params[1], kernel_size=params[2], stride=params[3]))
             encoder_modules.append(nn.ReLU())
         self.encoder_convs = nn.Sequential(*encoder_modules)
+        self.end_shape = (c2, 1, 60, 60) if nlayers==2 else (c3, 1, 27, 27)
+        if self.low_dim_rep is not None:
+          self.low_dim_mapping = nn.Sequential(nn.Linear(np.prod(self.end_shape), self.low_dim_rep), 
+                                               nn.Linear(self.low_dim_rep, np.prod(self.end_shape)))
 
         decoder_modules = []
-        for params in conv_params[:nlayers][::-1]:
+        for params in conv_params[nlayers][::-1]:
             decoder_modules.append(nn.ConvTranspose3d(params[1], params[0], kernel_size=params[2], stride=params[3]))
             decoder_modules.append(nn.ReLU())
         self.decoder_convs = nn.Sequential(*decoder_modules)
-
-        """
-        self.encoder_convs = nn.Sequential(nn.Conv3d(inchannels, c1, kernel_size=8, stride=2), nn.ReLU(), # 125
-                                          nn.Conv3d(c1, c2, kernel_size=7, stride=2), nn.ReLU(), nn.ReLU(), # 60
-                                          nn.Conv3d(c2, c3, kernel_size=8, stride=2), nn.ReLU(), # 27
-                                          nn.Conv3d(c3, c4, kernel_size=7, stride=2), nn.ReLU(),  # 11
-                                          nn.Conv3d(c4, c5, kernel_size=5, stride=2), nn.ReLU())  # 4
-
-        """
-
+        
     def forward(self, x):
         x = self.encoder_convs(x)
+        if self.low_dim_rep is not None:
+          x = self.low_dim_mapping(x.view(x.shape[0], -1)).view(-1, *self.end_shape)
         x = self.decoder_convs(x)
-
+        
         return x
 
 class TemporalConvAE2(nn.Module):
