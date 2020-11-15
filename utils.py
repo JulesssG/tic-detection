67791404451317c65f4ab0f49bc3bcdcd126cec4
Jules Gottraux <jules.gottraux@epfl.ch>
2020-11-15 17:20:01 +0000

    Add spectral normalization to subspace calculation, improve function

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import torch
+from torch import nn
 import datetime
 from matplotlib import pyplot as plt
 from scipy.linalg import solve_sylvester
@@ -102,18 +103,49 @@ def sec2string(sec):
     
     return str(datetime.timedelta(seconds=secr)).strip("00:")
 
-def subspace_angles(As, C1=None, C2=None, p=None):
-    nA, n1, n2 = As.shape
-    if nA != 2 or n1 != n2:
-        print('Function expect an array with exactly two matrix A of size nxn.')
-        return
-    n = n1
+def subspace_angles(A1, A2, C1=None, C2=None, **kwargs):
+    """
+    Expect C1 and C2 tensor or numpy array or None. If both are None expect a
+    named argument p to know the dimension of the frames.
     
+    Expect A1, A2 to be numpy array or tensors. If Ai is passed as model such
+    as FramePredictor it expects a named argument Ai_key, the key in the 
+    state_dict of the model.
+    """
+    # Extract A's
+    if isinstance(A1, nn.Module):
+        A1 = A1.state_dict[kwargs['A1_key']]
+    if isinstance(A2, nn.Module):
+        A2 = A2.state_dict[kwargs['A2_key']]
+    n1, n2 = A1.shape, A2.shape
+    if n1 != n2 or n1[0] != n1[1]:
+        print('Matrix A must be of same order and square.')
+        return
+    n = n1[0]
+    if isinstance(A1, torch.Tensor):
+        A1 = A1.detach().cpu().numpy()
+    if isinstance(A2, torch.Tensor):
+        A2 = A2.detach().cpu().numpy()
+    
+    # Extract C's
+    p = C1.shape[0] if C1 is not None else C2.shape[0] if C2 is not None else kwargs['p']
     if C1 is None:
         C1 = np.eye(p, M=n)
+    elif isinstance(C1, torch.Tensor):
+        C1 = C1.detach().cpu().numpy()
     if C2 is None:
         C2 = np.eye(p, M=n)
+    elif isinstance(C2, torch.Tensor):
+        C2 = C2.detach().cpu().numpy()
     Cs = np.array([C1, C2])
+    
+    # Normalize A's
+    norms = np.linalg.norm(np.stack((A1, A2)), ord=2, axis=(1,2))
+    if norms[0] > 1:
+        A1 = 0.98*A1/norms[0]
+    if norms[1] > 1:
+        A2 = 0.98*A2/norms[1]
+    As = np.array([A1, A2])
     
     Ps, Ps_ = np.zeros((2, 2, n, n)), np.zeros((2, n, n))
     for i in range(2):
